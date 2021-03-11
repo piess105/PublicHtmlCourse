96a022b0051d53420c5956a89caea005f356418a
Test <piess105@gmail.com>
2021-03-11 17:27:11 +0100

    Dodanie do repo

---
+++ js/script.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    window.scrollY = 100;
+
+    // const nav = document.querySelector("#navbar");
+    // const navLinks = document.querySelectorAll('.nav-link');
+    // const navbarNavAltMarkup = document.querySelector('#navbarNavAltMarkup');
+
+    // function tryAddShadow() {
+    //     if (window.scrollY >= 250) {
+    //         nav.classList.add('shadow-bg');
+    //     }
+    //     else {
+    //         nav.classList.remove('shadow-bg');
+    //     }
+    // }
+
+    // window.addEventListener('scroll', tryAddShadow);
+    
+    // navLinks.forEach(x => {
+
+    //     x.addEventListener('click', function () { 
+    //         navbarNavAltMarkup.classList.remove("show");
+    //      });
+    // })
+});
+
+
